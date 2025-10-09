eec4baa2553fdfe4896085f7487ede98ba01b34c
lanakbn <lana.kbn@gmail.com>
2025-10-09 18:50:43 +0200

    added back to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -36,15 +36,16 @@ function updateCity(event) {
   let citiesElement = document.querySelector(".cities");
 
   citiesElement.innerHTML = `
-    <div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
-    </div>
+  </div>
+  <a href="/">All cities</a>
   `;
 }
 
