78347fa093f264acf8534bfc6c46dea1819ec83f
lanakbn <lana.kbn@gmail.com>
2025-10-09 17:48:58 +0200

    added moment js with timezone

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,27 @@
+function updateTime() {
+  let genevaElement = document.querySelector("#geneva");
+  if (genevaElement) {
+    let genevaDateElement = genevaElement.querySelector(".date");
+    let genevaTimeElement = genevaElement.querySelector(".time");
+    let genevaTime = moment().tz("Europe/Geneva");
 
+    genevaDateElement.innerHTML = genevaTime.format("MMMM	Do YYYY");
+    genevaTimeElement.innerHTML = genevaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM	Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+updateTime();
+setInterval(updateTime, 1000);
